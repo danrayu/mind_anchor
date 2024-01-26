bdea9b206661bf1198a457dec8f3ba093821320b
Daniil Rayu <dan.rayu@gmail.com>
2024-01-26 10:50:05 +0100

    adding global types, minor field rename in prisma

--- app/types.d.ts
+++ app/types.d.ts
@@ -14,6 +14,27 @@ declare global {
     favorite: boolean;
     categories: Category[];
   }
+  type CollectionMeme = {
+    id: number;
+    meme: Meme;
+    index: number;
+    collectionId: number
+  }
+  type Collection = {
+    id: number;
+    title: string;
+    authorId: string;
+    memes: CollectionMeme[];
+  }
+  type Mindscape = {
+    id: number;
+    createdAt: Date;
+    updatedAt: Date;
+    title: string;
+    description: string;
+    authorId: string;
+    collections: Collection[];
+  }
 }
 
 export {};
