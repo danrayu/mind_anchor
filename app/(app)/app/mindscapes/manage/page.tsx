2533cde96a59f9014eea6452f8aef1ead114a433
Daniil Rayu <dan.rayu@gmail.com>
2024-02-04 19:03:06 +0100

    added mindscape schedule config model to prisma schema

--- app/(app)/app/mindscapes/manage/page.tsx
+++ app/(app)/app/mindscapes/manage/page.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 
 function page() {
   return (
+    
     <div>Manage mindscapes page</div>
   )
 }
---
+++ prisma/migrations/20240204180237_added_schedule_config_model/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE `MindscapeScheduleConfig` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `config` TEXT NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
